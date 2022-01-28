74ca3c807567d72a6dd407f8782d6991c228882f
fbtwitter <rzaugusdi354@gmail.com>
2022-01-28 15:49:20 +0700

    add redirect uri

--- src/components/SpotifyService/spotifyService.js
+++ src/components/SpotifyService/spotifyService.js
@@ -1,7 +1,7 @@
-import { AUTH_ENDPOINT, CLIENT_ID, RESPONSE_TYPE, SCOPE } from './spotifyApi';
+import { AUTH_ENDPOINT, CLIENT_ID, REDIRECT_URI, RESPONSE_TYPE, SCOPE } from './spotifyApi';
 
 export const handleLogin = () => {
-    window.location = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}&show_dialog=true`;
+    window.location = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}&show_dialog=true`;
 };
 
 export const getTokenFromUrl = () => {
